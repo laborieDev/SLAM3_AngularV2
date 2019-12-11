1016305f06ee244f0e922f2e3ab293622c192e2d
Anthony <acs.agl46@gmail.com>
2019-12-11 12:21:09 +0100

    Modifications page accueil

--- angular-laborie/src/app/app.component.ts
+++ angular-laborie/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'my-app',
+  template : '<h1>Gestion des rapports de visite</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
