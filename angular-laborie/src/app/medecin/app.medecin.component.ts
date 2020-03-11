c52a7331c70d0a2f5c3f6f9b863668ea18862d68
Anthony <acs.agl46@gmail.com>
2020-03-11 11:59:59 +0100

    A finir

--- angular-laborie/src/app/medecin/app.medecin.component.ts
+++ angular-laborie/src/app/medecin/app.medecin.component.ts
@@ -1,8 +1,72 @@
 import { Component } from '@angular/core';
+import { DataService } from '../services/app.services.data';
+
 @Component({
   selector: 'app-medecin',
   templateUrl : 'app.medecin.html'
 })
 export class Medecin {
   legende : string="Rechercher le médecin...";
+  lesMedecins : Array<any>;
+  lesRapports : Array<any>;
+  nomMedecin : string;
+  medecin : any;
+
+  lblMessage : string = "Modification effectuée";
+
+  //gestion affichage des divs
+  noSecondNavbar : boolean = true;
+  afficherMedecins : boolean = false;
+  afficherRapports : boolean = false;
+  afficherMedecin : boolean = false;
+  afficherMessage : boolean = false;
+
+  constructor( private dataService : DataService){}
+
+  charger() : void{
+    this.dataService.chargerMedecins(this.nomMedecin)
+                    .subscribe( 
+                      (data)=>{this.lesMedecins = data;}       
+                      ,(error)=>{}
+                    );
+    this.afficherMedecins = true;
+  }
+
+  selectionner(medecin) : void{
+    this.medecin = medecin;
+    this.noSecondNavbar = false;
+    this.dataService.chargerRapports(medecin.id)
+                    .subscribe( 
+                      (data)=>{this.lesRapports = data;}       
+                      ,(error)=>{}
+                    );
+    this.afficherMedecins = false;
+    this.nomMedecin = medecin.nom+" "+medecin.prenom;
+    
+  }
+
+  derniersRapports(): void{
+    this.afficherRapports = true;
+    this.afficherMedecin = false;
+  }
+
+  estCacheMenu():boolean{
+    return this.noSecondNavbar;
+  }
+
+  majMedecin(): void{
+    this.afficherMedecin = true;
+    this.afficherRapports = false;
+    this.afficherMessage = false;
+  }
+
+  validerMaJMedecin(): void{
+
+    this.dataService.majMedecin(this.medecin.id, this.medecin.adresse, this.medecin.tel, this.medecin.spe)
+                    .subscribe( 
+                      (data)=>{}       
+                      ,(error)=>{}
+                    );
+    this.afficherMessage = true;
+  }
 }
--- angular-laborie/src/app/services/app.services.data.ts
+++ angular-laborie/src/app/services/app.services.data.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
-    private  urlDomaine :  string = "http://localhost:8888/restGSB/";
+    private  urlDomaine :  string = "http://localhost:8888/restGSB";
     constructor(private http: Http) {}
     public connexion( loginIn : string, mdpIn : string ) : Observable<string[]> {
          let url :string = this.urlDomaine + "/connexion?login=" + loginIn + "&mdp=" + mdpIn;
@@ -15,4 +15,27 @@ export class DataService {
          return req;
     }
 
+    public chargerMedecins(nomMedecin : string){
+        let url :string = this.urlDomaine + "/medecins?nom=" + nomMedecin;
+        let req = this.http
+                     .get(url)
+                     .map((r: Response)=>r.json());
+        return req;
+    }
+
+    public chargerRapports(idMedecin : Number ): Observable<string[]>{
+        let url :string = this.urlDomaine + "/rapports/" + idMedecin;
+         let req = this.http
+                     .get(url)
+                     .map((r: Response)=>r.json());
+         return req;
+    }
+
+    public majMedecin(id : string ,adresse : string, tel :string, spe : string) {
+        let url : string =  this.urlDomaine + "/majmedecin?idmedecin=" + id + "&adresse=";
+            url += adresse + "&tel=" + tel +"&specialite=" + spe;
+        let req = this.http
+                   .get(url)
+     return req;
+    }
 }
